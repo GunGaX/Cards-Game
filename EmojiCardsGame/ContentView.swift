4709d76d5905565753d7096abcf022fc469ba807
GunGaX <118834936+GunGaX@users.noreply.github.com>
2023-03-02 01:24:19 +0200

    Initial changes

--- EmojiCardsGame/ContentView.swift
+++ EmojiCardsGame/ContentView.swift
@@ -8,33 +8,100 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🦈", "🦋", "🐠", "🦚", "🌵", "🍀", "🌳", "🍁", "🌖", "✨", "☀️", "❄️", "⛈️", "🍑", "🍎", "🍊", "🍐", "🍕", "🏀", "🎾", "🏐", "🌸", "🌼"]
+    @State var emojiCount = 4
+    
     var body: some View {
-        HStack {
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(3/4, contentMode: .fit)
+                    }
+                }
+                .padding()
+            }
+            .foregroundColor(.blue)
+            
+            Spacer()
+            
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .padding(.horizontal)
         }
-        .padding()
-        .foregroundColor(.blue)
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.square")
+                .font(.largeTitle)
+                .padding(.horizontal)
+        }
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount != 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.square")
+                .font(.largeTitle)
+                .padding(.horizontal)
+        }
     }
 }
+
+struct CardView: View {
+    @State var isFaceUp: Bool = false
+    var content: String
+    
+    var body: some View {
+        ZStack {
+            let rectangle = RoundedRectangle(cornerRadius: 20, style: .continuous)
+
+            if isFaceUp {
+                rectangle
+                    .fill()
+                    .foregroundColor(.white)
+                rectangle
+                    .stroke(lineWidth: 4)
+                Text(content)
+                    .font(.largeTitle)
+            }
+            else {
+                rectangle
+                    .fill()
+                rectangle
+                    .stroke(lineWidth: 4)
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-
-
-struct CardView: View {
-    var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .fill()
-                .foregroundColor(.white)
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 4)
-        }
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
